65b6f242c808c5316872beab378bc40ba9cb3842
guptaNswati <sgupta1505@gmail.com>
2017-01-02 22:27:56 +0000

    write to stderr in pythin

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import sys
+str = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
+#ref:http://stackoverflow.com/questions/5574702/how-to-print-to-stderr-in-python
+sys.stderr.write(str)
+sys.exit(1)
