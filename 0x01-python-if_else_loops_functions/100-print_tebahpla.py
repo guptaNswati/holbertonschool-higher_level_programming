ca7de1a2a43447d102119f4b828200a9b5ff8325
guptaNswati <sgupta1505@gmail.com>
2017-01-03 23:41:20 +0000

    added string formatter

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -5,7 +5,7 @@ while i >= 97:
     if i % 2 != 0:
         i = i - 32
         flag = 1
-    print(chr(i), end="")
+    print("{:s}".format(chr(i)), end="")
     if flag == 1:
         i = i + 32
     i = i - 1
