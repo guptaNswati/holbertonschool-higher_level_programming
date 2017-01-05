5009e8a804e76f14d8eb70301cdf9aa7b3676256
guptaNswati <sgupta1505@gmail.com>
2017-01-05 19:54:45 +0000

    v1 0

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+from add_0 import add
+
+if __name__ == "__main__":
+    add(1, 2)
--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def add(a, b):
     print("{:d} + {:d} = {:d}".format(a, b, a + b))
-
-if __name__ == "__main__":
-    import sys
-    add(int(sys.argv[1]), int(sys.argv[2]))
+    return (a + b)
