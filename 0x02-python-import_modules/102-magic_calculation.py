986f1d0aa929ef2d3696fe118d241d16965da121
guptaNswati <sgupta1505@gmail.com>
2017-01-06 06:55:47 +0000

    added dis

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+import dis
+
+
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
     else:
         sub(a, b)
+
+
+dis.dis(magic_calculation)
