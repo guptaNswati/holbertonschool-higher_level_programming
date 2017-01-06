8b8de316b089718013e1b799b4cd40be6a8c0af6
guptaNswati <sgupta1505@gmail.com>
2017-01-06 07:09:31 +0000

    added loop

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -6,8 +6,11 @@ def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
+        for n in range(4, 6):
+            c = add(c, n)
+        return c
     else:
-        sub(a, b)
+        return sub(a, b)
 
 
 dis.dis(magic_calculation)
