928ee64e385a93ec036e22c7910abf523afeae42
guptaNswati <sgupta1505@gmail.com>
2017-01-06 01:47:41 +0000

    103

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from alphabet import print_alp
-    print_alp(65)
+import string
+print(string.ascii_uppercase)
--- 0x02-python-import_modules/alphabet.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-def print_alp(i):
-    if i < 65 or i > 90:
-        return
-    repr(chr(i))
-    print_alp(i+1)
