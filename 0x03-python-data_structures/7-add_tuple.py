eba6ebd38ad41aca225757c8f15b22fc5d3719ce
guptaNswati <sgupta1505@gmail.com>
2017-01-10 20:26:06 +0000

    python 3 6,7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) == 0:
+        tuple_a = 0, 0
+    if len(tuple_b) == 0:
+        tuple_b = 0, 0
+    if len(tuple_a) == 1:
+        tuple_a = tuple_a[0], 0
+    if len(tuple_b) == 1:
+        tuple_b = tuple_b[0], 0
+    new_list = []
+    i = 0
+    while i < 2:
+        if tuple_a[i] == None:
+            first = 0
+        else:
+            first = tuple_a[i]
+        if tuple_b[i] == None:
+            second = 0
+        else:
+            second = tuple_b[i]
+        third = first + second
+        new_list.append(third)
+        i += 1
+    new_tup = new_list[0], new_list[1]
+    return new_tup
