168b3b585b1eaeb2b35c4977094efc83f3b59d02
guptaNswati <sgupta1505@gmail.com>
2017-01-12 23:47:55 +0000

    pep8 fixes and removing all repeating values in dic

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(my_dict, value):
-    v = list(my_dict.values())
-    key = list(my_dict.keys())
-    my_dict.pop(key[v.index(value)], None)
+    for k in list(my_dict.keys()):
+        if my_dict[k] == value:
+            my_dict.pop(k, None)
     return my_dict
