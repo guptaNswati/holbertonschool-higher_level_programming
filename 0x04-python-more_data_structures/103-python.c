de7cb61ee4e034aba5af4e5105b642870881afcd
guptaNswati <sgupta1505@gmail.com>
2017-01-13 04:22:06 +0000

    103

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
 #include <Python.h>
 
-/**
-* print_pyhton_list - print some basic info about Python lists
-* @p: python object
-* Return: nothing
-**/
-void print_python_list(PyObject *p)
-{
-	printf("%p\n", (void *)p);
-}
 
 /**
 * print_python_bytes -
@@ -19,10 +10,41 @@ void print_python_list(PyObject *p)
 void print_python_bytes(PyObject *p)
 {
 	char *s;
-	Py_ssize_t len;
+	Py_ssize_t len, i;
 
 	printf("[.] bytes object info\n");
+	if (!p)
+		printf("[ERROR] Invalid Bytes Object\n");
 	PyBytes_AsStringAndSize(p, &s, &len);
 	printf("size: %d\n", (int)len);
 	printf("trying string: %s\n", s);
+	if (len > 10)
+		len = 10;
+	else
+		len++;
+	printf("first %d bytes: ", (int)len);
+	for (i = 0; i < len - 1; i++)
+		printf("%02x ", s[i]);
+	printf("%02x\n",s[len - 1]);
+}
+
+
+/**
+* print_pyhton_list - print some basic info about Python lists
+* @p: python object
+* Return: nothing
+**/
+void print_python_list(PyObject *p)
+{
+/*	char *s;
+	Py_ssize_t len, i; */
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %d\n", (int)PyBytes_Size(p));
+	printf("[*] Allocated = %d\n", (int)((PyListObject *)p)->allocated);
+/*	for (i = 0; i < len; i++)
+	{
+		printf("Element %d: bytes\n", (int)i);
+		print_python_bytes((PyObject *)PyBytes_FromString(s[i]));
+		} */
 }
