98b308d50dc99b976350bbe4e68f8b77c0e09292
guptaNswati <sgupta1505@gmail.com>
2017-01-24 00:16:30 +0000

    corrections in linkedlist

--- 0x07-python-classes/100-singly_linked_list.py
+++ 0x07-python-classes/100-singly_linked_list.py
@@ -35,13 +35,36 @@ class SinglyLinkedList:
     def sorted_insert(self, value):
         newNode = Node(value, None)
         if self.__head is None:
-            self__.head = newNode
+            self.__head = newNode
+        elif self.__head.next is None:
+            if self.__head.data < value:
+                self.__head.next = newNode
+            elif self.__head.data >= value:
+                newNode.next = self.__head
+                self.__head = new
         else:
             tmp = self.__head
-            while(tmp.next):
-                if tmp.data < value and tmp.next.data > value:
+            while tmp.next:
+                if tmp.data >= value:
+                    newNode.next = tmp
+                    tmp = newNode
+                    self.__head = tmp
+                    return
+                elif tmp.data < value and tmp.next.data >= value:
                     newNode.next = tmp.next
                     tmp.next = newNode
                     return
                 tmp = tmp.next
             tmp.next = newNode
+
+    def __str__(self):
+        if not self.__head is None:
+            tmp = self.__head
+            out = "" + str(tmp.data) + "\n"
+            while tmp.next:
+                tmp = tmp.next
+                if tmp.next:
+                    out += str(tmp.data) + "\n"
+                else:
+                    out += str(tmp.data)
+            return out
