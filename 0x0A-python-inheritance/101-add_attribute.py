0c26c66830f090102f6e7a095bc07148a1c37541
guptaNswati <sgupta1505@gmail.com>
2017-01-31 07:47:08 +0000

    fixes

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def add_attribute(obj, name, value):
-    __slots__ = "{}".format(name)
-    setattr(obj, name, value)
-    else:
-        raise TypeError("can't add new attribute")
+    obj.__dict__[name] = value
