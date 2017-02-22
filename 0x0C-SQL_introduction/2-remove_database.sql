c6bd5d9fcec0a4bf2fa2565e189e4d2e7d2e5cf8
guptaNswati <sgupta1505@gmail.com>
2017-02-22 21:19:35 +0000

    added not failing clause

--- 0x0C-SQL_introduction/2-remove_database.sql
+++ 0x0C-SQL_introduction/2-remove_database.sql
@@ -1,4 +1,4 @@
 -- Deletes the database hbtn_0c_0
 -- Should not fail if database doesn't exists
 -- Not allowed to use the SELECT or SHOW
-DROP DATABASE hbtn_0c_0;
+DROP DATABASE IF EXISTS hbtn_0c_0;
