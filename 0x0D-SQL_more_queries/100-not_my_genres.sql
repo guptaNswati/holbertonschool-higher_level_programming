2d87038b7e93e2216286a78bb3e6ba3fd94d65a2
guptaNswati <sgupta1505@gmail.com>
2017-03-01 04:55:17 +0000

    updated 100 and 101 for last check

--- 0x0D-SQL_more_queries/100-not_my_genres.sql
+++ 0x0D-SQL_more_queries/100-not_my_genres.sql
@@ -1,7 +1,8 @@
 -- lists all genres not linkde to show Dexter
 -- results must be sorted in ascending order by genre name
 -- database name will be passed as an argument of the mysql command
-SELECT tv_genres.name FROM tv_show_genres
+SELECT DISTINCT tv_genres.name FROM tv_genres
+WHERE tv_genres.id NOT IN (SELECT DISTINCT tv_genres.id FROM tv_show_genres
 INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE NOT tv_shows.title='Dexter' ORDER BY name ASC;
+WHERE tv_shows.title = 'Dexter') ORDER BY name ASC;
--- 0x0D-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0D-SQL_more_queries/101-not_a_comedy.sql
@@ -1,7 +1,8 @@
 -- lists all shows except comedy
 -- results must be sorted in ascending order by tv_shows.title
 -- database name will be passed as an argument of the mysql command
-SELECT tv_shows.title FROM tv_genres
+SELECT tv_shows.title FROM tv_shows
+WHERE tv_shows.title NOT IN (SELECT tv_shows.title FROM tv_genres
 INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE NOT tv_genres.name='Comedy' ORDER BY tv_shows.title ASC;
+WHERE tv_genres.name = 'Comedy') ORDER BY tv_shows.title ASC;
