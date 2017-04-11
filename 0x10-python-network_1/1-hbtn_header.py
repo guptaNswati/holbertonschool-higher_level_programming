f3a06ba8542d0285619bf53f088df288beede16a
guptaNswati <sgupta1505@gmail.com>
2017-04-11 18:29:35 +0000

    fetch and head

---
+++ 0x10-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module takes in a URL, sends a request to the URL and displays the value of
+the X-Request-Id variable found in the header of the response.
+"""
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    page, header = urllib.request.urlretrieve(sys.argv[1])
+    print(header['X-Request-Id'])
