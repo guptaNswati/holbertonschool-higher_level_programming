1524998c8522e4c63c5f27055aa831ec40284803
guptaNswati <sgupta1505@gmail.com>
2017-04-11 05:49:17 +0000

    execution permision

--- 0x0F-python-network_0/102-catch_me.sh
+++ 0x0F-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that do a put request using curl
+# script that do a put request using header origin with curl
 curl -Ls -H "Origin: HolbertonSchool" -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me
