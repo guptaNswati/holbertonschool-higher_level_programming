6895e1a5eea64aea536296a39e869c8c109cab9b
guptaNswati <sgupta1505@gmail.com>
2017-04-12 03:39:40 +0000

    fixing 8

--- 0x10-python-network_1/8-json_api.py
+++ 0x10-python-network_1/8-json_api.py
@@ -8,27 +8,18 @@ import sys
 
 
 if __name__ == "__main__":
-    arg_len = len(sys.argv)
-    if arg_len > 2:
-        print("No result")
-        sys.exit()
-    if arg_len == 2:
-        if len(sys.argv[1]) > 1:
-            print("No result")
-            sys.exit()
-        try:
-            letter = int(sys.argv[1])
-            print("No result")
-            sys.exit()
-        except (TypeError, ValueError):
-            letter = sys.argv[1]
-    elif arg_len == 1:
+    if len(sys.argv) > 1:
+        letter = sys.argv[1]
+    else:
         letter = ""
     payload = {'q': letter}
     req = requests.post('http://0.0.0.0:5000/search_user', data=payload)
-    content_type = req.headers['Content-Type']
-    content_type = content_type[len(content_type)-4:]
-    if content_type == 'json' and req.text is not None:
-        print("[{:d}] {:s}".format(req.text['id'], req.text['name']))
-    else:
+    try:
+        content = req.json()
+    except ValueError:
         print("Not a valid JSON")
+        sys.exit()
+    if content:
+        print("[{}] {}".format(content.get('id'), content.get('name')))
+    else:
+        print("No result")
