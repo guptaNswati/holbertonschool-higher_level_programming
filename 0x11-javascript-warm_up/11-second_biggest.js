e546687d144f1900e6330b2ce50a34ce49b9a1e9
guptaNswati <sgupta1505@gmail.com>
2017-04-28 03:57:09 +0000

    refractored

--- 0x11-javascript-warm_up/11-second_biggest.js
+++ 0x11-javascript-warm_up/11-second_biggest.js
@@ -1,28 +1,20 @@
 #!/usr/bin/node
 
 function secondMax (arr) {
-  let currentMax = arr[0];
   let maxSoFar = arr[0];
+  let currentMax = arr[0];
   for (let i = 1; i < arr.length; i++) {
-    if (currentMax > maxSoFar) {
-      let temp = maxSoFar;
-      maxSoFar = currentMax;
-      currentMax = temp;
-    }
-    if (arr[i] > currentMax) {
-      currentMax = arr[i];
+    if (maxSoFar < arr[i]) {
+      currentMax = maxSoFar;
+      maxSoFar = arr[i];
     }
   }
-  if (currentMax < maxSoFar) {
-    console.log(currentMax);
-  } else {
-    console.log(maxSoFar);
-  }
+  console.log(currentMax);
 }
 
 let myVar = process.argv.slice(2);
 if (myVar.length > 1) {
   secondMax(myVar);
 } else {
-  console.log('0');
+  console.log(0);
 }
