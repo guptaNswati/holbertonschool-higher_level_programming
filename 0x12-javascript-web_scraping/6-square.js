f0e59209a81677f34d232996f4086f7ddeb82fec
guptaNswati <sgupta1505@gmail.com>
2017-05-01 03:56:31 +0000

    ipdated square

--- 0x12-javascript-web_scraping/6-square.js
+++ 0x12-javascript-web_scraping/6-square.js
@@ -4,14 +4,13 @@ Square.prototype.charPrint = function (c) {
   if (c === undefined) {
     this.print();
   } else {
-    for (let j = 0; j < this.size; j++) {
+    for (let j = 0; j < this.height; j++) {
       let squarePrint = '';
-      for (let k = 0; k < this.size; k++) {
+      for (let k = 0; k < this.width; k++) {
         squarePrint += c;
       }
       console.log(squarePrint);
     }
   }
 };
-
 exports.Square = Square;
