5408805e5c9cfa9f9d574bf3fc5c69580b49ec95
guptaNswati <sgupta1505@gmail.com>
2017-05-01 03:59:12 +0000

    5

--- 0x12-javascript-web_scraping/5-square.js
+++ 0x12-javascript-web_scraping/5-square.js
@@ -2,6 +2,8 @@
 
 const Rectangle = require('./4-rectangle').Rectangle;
 
-exports.Square = function Square (size) {
+function Square (size) {
   Rectangle.call(this, size, size);
 };
+
+exports.Square = Square;
