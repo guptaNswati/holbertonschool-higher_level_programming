a7018a0136cc64b0e4b782ee5f23591099804e73
guptaNswati <sgupta1505@gmail.com>
2017-05-01 07:04:27 +0000

    updated format

--- 0x12-javascript-web_scraping/103-search_twitter.js
+++ 0x12-javascript-web_scraping/103-search_twitter.js
@@ -4,11 +4,6 @@ let request = require('request');
 let base64 = require('base-64');
 let utf8 = require('utf8');
 
-let arg = process.argv[4];
-arg = arg.slice(0, 0) + arg.slice(1, arg.length);
-
-let url = 'https://api.twitter.com/1.1/search/tweets.json?q=%23' + arg;
-
 let oauth = process.argv[2] + ':' + process.argv[3];
 oauth = utf8.encode(oauth);
 oauth = base64.encode(oauth);
@@ -33,11 +28,17 @@ request.post(tokens, function (error, response, body) {
   if (error) {
     console.log(error);
   }
+
   let data = JSON.parse(body);
   let access = data['access_token'];
   let searchHead = {
     'Authorization': 'Bearer ' + access
   };
+
+  let arg = process.argv[4];
+  arg = arg.slice(0, 0) + arg.slice(1, arg.length);
+  let url = 'https://api.twitter.com/1.1/search/tweets.json?q=%23' + arg;
+
   request.get({url: url, headers: searchHead}, function (error, response, body) {
     if (error) {
       console.log(error);
