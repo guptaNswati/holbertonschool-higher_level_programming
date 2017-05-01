2c518dd9489c06563e1be30c05c7d0c9b34c4df5
guptaNswati <sgupta1505@gmail.com>
2017-05-01 02:44:08 +0000

    updated 4

--- 0x12-javascript-web_scraping/4-rectangle.js
+++ 0x12-javascript-web_scraping/4-rectangle.js
@@ -1,25 +1,26 @@
 #!/usr/bin/node
+
 exports.Rectangle = function Rectangle (w, h) {
   if (w > 0 && h > 0) {
     this.width = w;
     this.height = h;
-    this.print = function () {
-      for (let i = 0; i < h; i++) {
-        let rectPrint = '';
-        for (let j = 0; j < w; j++) {
-          rectPrint += 'X';
-        }
-        console.log(rectPrint);
-      }
-    };
-    this.rotate = function () {
-      var tmp = this.width;
-      this.width = this.height;
-      this.height = tmp;
-    };
-    this.double = function () {
-      this.width *= 2;
-      this.height *= 2;
-    };
   }
+  this.print = function () {
+    for (let i = 0; i < this.height; i++) {
+      let rectPrint = '';
+      for (let j = 0; j < this.width; j++) {
+        rectPrint += 'X';
+      }
+      console.log(rectPrint);
+    }
+  };
+  this.rotate = function () {
+    let tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  };
+  this.double = function () {
+    this.width *= 2;
+    this.height *= 2;
+  };
 };
